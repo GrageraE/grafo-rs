40abe08facc97471879b0c2f347a80103de4d9ea
GrageraE <44318798+GrageraE@users.noreply.github.com>
2024-03-06 19:28:00 +0100

    Adding functionalities
    Signed-off-by: GrageraE <44318798+GrageraE@users.noreply.github.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,63 +1,115 @@
 pub mod grafo_rs
 {
-    pub enum Arista<Vertice>
-    where Vertice: PartialEq, Vertice: Clone {
-        Arista(Vertice, Vertice),
+    pub struct NoPeso;
+
+    impl PartialEq for NoPeso {
+        fn eq(&self, _: &Self) -> bool {
+            true
+        }
+    }
+
+    impl Clone for NoPeso {
+        fn clone(&self) -> Self {
+            Self
+        }
+    }
+
+    pub enum Arista<Vertice, Peso = NoPeso>
+    where Vertice: PartialEq, Vertice: Clone, Peso: PartialEq, Peso: Clone {
+        Arista(Vertice, Vertice, Option<Peso>),
         VerticeAislado(Vertice)
     }
 
-    impl<Vertice> Arista<Vertice> 
-    where Vertice: PartialEq, Vertice: Clone {
-        pub fn arista(v: Vertice, w: Vertice) -> Self
+    impl<Vertice, Peso> Arista<Vertice, Peso> 
+    where Vertice: PartialEq, Vertice: Clone, Peso: PartialEq, Peso: Clone {
+        pub fn arista(v: Vertice, w: Vertice, p: Option<Peso>) -> Self
         {
-            Self::Arista(v, w)
+            Self::Arista(v, w, p)
+        }
+
+        pub fn arista_sin_peso(v: Vertice, w: Vertice) -> Self
+        {
+            Self::Arista(v, w, None)
         }
 
         pub fn vertice(v: Vertice) -> Self
         {
             Self::VerticeAislado(v)
         }
+
+        pub fn peso_por_defecto(&self) -> Option<Arista<Vertice, u8>>
+        {
+            match &self {
+                Self::Arista(v, w, _) => Some(Arista::Arista(v.clone(), w.clone(), Some(1))),
+                _ => None
+            }
+        }
+
+        pub fn arista_contiene_vertice(&self, v0: &Vertice) -> bool
+        {
+            match &self {
+                Self::Arista(v, w, _) => *v == *v0 || *w == *v0,
+                _ => false
+            }
+        }
+
+        pub fn get_vertices(&self) -> Option<(&Vertice, &Vertice)>
+        {
+            if let Self::Arista(v, w, _) = &self
+            {
+                return Some((v, w));
+            }
+            None
+        }
     }
 
-    impl<Vertice> Clone for Arista<Vertice> 
-    where Vertice: PartialEq, Vertice: Clone {
+    impl<Vertice, Peso> Clone for Arista<Vertice, Peso> 
+    where Vertice: PartialEq, Vertice: Clone, Peso: PartialEq, Peso: Clone {
         fn clone(&self) -> Self {
             match &self {
-                Arista::Arista(v, w) => Self::Arista(v.clone(), w.clone()),
+                Arista::Arista(v, w, p) => 
+                    Self::Arista(v.clone(), w.clone(), p.clone()),
                 Arista::VerticeAislado(v) => Self::VerticeAislado(v.clone())
             }
         }
     }
 
-    impl<Vertice> PartialEq for Arista<Vertice> 
-    where Vertice: PartialEq, Vertice: Clone {
+    impl<Vertice, Peso> PartialEq for Arista<Vertice, Peso> 
+    where Vertice: PartialEq, Vertice: Clone, Peso: PartialEq, Peso: Clone {
         fn eq(&self, other: &Self) -> bool {
-            if let Self::Arista(v1, w1) = &self
-            {
-                if let Self::Arista(v2, w2) = other
-                {
-                    return (v1 == w1 && v2 == w2) || (v1 == w2 && v2 == w1);
-                }
-                else 
-                {
-                    return false;
+            match &self {
+                Self::Arista(v1, w1, p1) => {
+                    if let Self::Arista(v2, w2, p2) = other
+                    {
+                        return (v1 == v2 && w1 == w2 && p1 == p2) || (v1 == w2 && v2 == w1 && p1 == p2);
+                    }
+                    else 
+                    {
+                        return false;
+                    }
+                },
+                Self::VerticeAislado(v1) => {
+                    if let Self::VerticeAislado(v2) = other
+                    {
+                        return v1 == v2;
+                    }
+                    else 
+                    {
+                        return false;
+                    }
                 }
             }
-            if let Self::VerticeAislado(w) = other
-            {
-                unimplemented!();
-            }
-            return false;
         }
     }
 
-    pub struct Grafo<Vertice> 
-    where Vertice: PartialEq, Vertice: Clone {
-        lista_aristas: Vec<Arista<Vertice>>,
+    pub struct Grafo<Vertice, Peso = NoPeso> 
+    where Vertice: PartialEq, Vertice: Clone, Peso: PartialEq, Peso: Clone {
+        lista_aristas: Vec<Arista<Vertice, Peso>>,
     }
 
-    impl<Vertice> Grafo<Vertice> 
-    where Vertice: PartialEq, Vertice: Clone {
+    impl<Vertice, Peso> Grafo<Vertice, Peso> 
+    where Vertice: PartialEq, Vertice: Clone, Peso: PartialEq, Peso: Clone {
+        /// Devuelve el numero de vertices
         pub fn size(&self) -> usize
         {
             unimplemented!();
@@ -70,10 +122,70 @@ pub mod grafo_rs
             }
         }
 
-        pub fn from_aristas(lista: &[Arista<Vertice>]) -> Self
+        pub fn from_aristas(lista: &[Arista<Vertice, Peso>]) -> Self
         {
+            // TODO: Revisar que todas las aristas sean diferentes
             Self{
                 lista_aristas: lista.to_vec()
+            }
+        }
+
+        pub fn add_aristas(&mut self, mut lista: Vec<Arista<Vertice, Peso>>)
+        {
+            self.lista_aristas.append(&mut lista);
+        }
+
+        pub fn add_vertices(&mut self, lista: Vec<Vertice>)
+        {
+            let mut lista_aristas: Vec<Arista<Vertice, Peso>> = lista.into_iter().map(|x| Arista::<Vertice, Peso>::VerticeAislado(x)).collect();
+            self.lista_aristas.append(&mut lista_aristas);
+        }
+
+        pub fn entorno(&self, v: &Vertice) -> Vec<&Vertice>
+        {
+            let mut res = vec![];
+            for arista in self.lista_aristas.iter()
+            {
+                if arista.arista_contiene_vertice(&v)
+                {
+                    let (v1, v2) = arista.get_vertices().unwrap();
+                    match *v == *v1 {
+                        true => { res.push(v2); },
+                        false => { res.push(v1); }
+                    }
+                }
+            }
+            res
+        }
+
+        pub fn grado(&self, v: &Vertice) -> Option<usize>
+        {
+            let mut encontrado = false;
+            let mut result: usize = 0;
+            for arista in self.lista_aristas.iter()
+            {
+                if arista.arista_contiene_vertice(&v)
+                {
+                    encontrado = true;
+                    result += 1;
+                    // Vemos si es un ciclo
+                    let (v1, v2) = arista.get_vertices().unwrap();
+                    if *v1 == *v2
+                    {
+                        result += 1;
+                    }
+                }
+                else if !encontrado
+                {
+                    if let Arista::VerticeAislado(v1) = arista
+                    {
+                        encontrado = *v == *v1;
+                    }
+                }
+            }
+            match encontrado {
+                true => Some(result),
+                _ => None
             }
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,27 @@ use grafo_rs::grafo_rs::Arista;
 use grafo_rs::grafo_rs::Grafo;
 
 #[test]
-fn test_creacion_grafo()
+fn test_grado()
 {
-    let grafo0 = Grafo::from_aristas(&[Arista::arista(1, 2),
-                                                        Arista::arista(2, 3),
+    let grafo0: Grafo<i32, grafo_rs::grafo_rs::NoPeso> = Grafo::from_aristas(&[Arista::arista_sin_peso(1, 2),
+                                                        Arista::arista_sin_peso(2, 3),
+                                                        Arista::VerticeAislado(5)]);
+    
+    assert_eq!(Some(0), grafo0.grado(&5));
+    assert_eq!(Some(1), grafo0.grado(&1));
+    assert_eq!(Some(2), grafo0.grado(&2));
+    assert_eq!(None, grafo0.grado(&20));
+}
+
+#[test]
+fn test_entorno()
+{
+    let grafo0: Grafo<i32, grafo_rs::grafo_rs::NoPeso> = Grafo::from_aristas(&[Arista::arista_sin_peso(1, 2),
+                                                        Arista::arista_sin_peso(2, 3),
                                                         Arista::VerticeAislado(5)]);
 
+    assert_eq!(vec![2], grafo0.entorno(&1).into_iter().map(|x| *x).collect::<Vec<i32>>());
+    assert_eq!(vec![1, 3], grafo0.entorno(&2).into_iter().map(|x| *x).collect::<Vec<i32>>());
 }
 
 fn main() {}

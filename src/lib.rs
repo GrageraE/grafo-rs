ca9f84ab6512397fe7a323d2e7c9a09f524eb59d
GrageraE <44318798+GrageraE@users.noreply.github.com>
2024-04-13 17:16:56 +0200

    Changed impl of arbol_busqueda
    Signed-off-by: GrageraE <44318798+GrageraE@users.noreply.github.com>

--- src/lib.rs
+++ src/lib.rs
@@ -358,81 +358,51 @@ pub mod grafo_rs
             Some(Arbol::<Vertice, Peso>::new(arbol, vertice_inicial.clone()))
         }
 
-        ///
-        /// PRE: Vertice del grafo
-        /// POST: Arbol de busqueda en profundidad y etiquetado df. None si el vertice no se encuentra
-        /// 
         pub fn arbol_profundidad(&self, v0: &Vertice) -> Option<(Arbol<Vertice, Peso>, Etiquetado<Vertice>)>
         {
             let mut arbol = Self::new();
             let mut df = Etiquetado::new(Some("df"));
-            let mut temp: Etiquetado<&Vertice> = Etiquetado::new(None);
-            // Definimos un vector con las aristas frontera de v0
-            let mut aristas_frontera = self.aristas_por_vertice(v0);
-            if aristas_frontera.is_empty()
-            {
-                return None;
-            }
-            // Definimos variables temporales
-            let mut vertice_visitado: &Vertice;
-            let mut vertices_visitados: Vec<&Vertice> = vec![v0];
-            // Inicializamos los etiquetados y el contador
-            df.add_vertice(v0.clone(), 0);
-            let mut i: isize = 1;
-            temp.add_vertice(v0, 0);
-            for adyacente in self.entorno(v0).unwrap().into_iter()
-            {
-                temp.add_vertice(adyacente, i);
-            }
 
-            while !aristas_frontera.is_empty() 
+            // Definimos una pila para backtracking y un vector de vertices visitados
+            let mut backtrack: Vec<&Vertice> = vec![];
+            let mut vertices_visitados: Vec<&Vertice> = vec![];
+            let mut vertice_visitado = v0;
+
+            // Definimos contador para df
+            let mut i: isize = 0;
+
+            while i == 0 || !backtrack.is_empty()
             {
-                // Seleccionamos la arista frontera cuyo vertice exterior tenga mayor etiquetado
-                let arista_elegida: Vec<&&Arista<Vertice, Peso>> = aristas_frontera.iter()
-                                                    .filter(|x| x.arista_contiene_vertice(temp.max().unwrap().get_vertice()))
-                                                    .collect();                            
-                assert!(arista_elegida.len() == 1);     // TODO: Provisional
-
-                let arista_elegida = *arista_elegida[0];
-
-                arbol.add_aristas(vec![arista_elegida.clone()]);
-                // Establecemos el nuevo vertice visitado
-                if vertices_visitados.contains(&arista_elegida.get_vertices().unwrap().0)
+                // Seleccionamos una arista al azar
+                let mut aristas_vecinas: Vec<&Arista<Vertice, Peso>> = self.lista_aristas.iter()
+                                                .filter(|x| x.arista_contiene_vertice(vertice_visitado))    
+                                                .collect();
+                
+                for vertice in vertices_visitados.iter().filter(|x| **x != vertice_visitado)
                 {
-                    vertice_visitado = arista_elegida.get_vertices().unwrap().1;
+                    aristas_vecinas.retain(|x| !x.arista_contiene_vertice(vertice));
+                }
+
+                // Añadimos vertice al etiquetado
+                // TODO: Evitar contaminar el etiquetado y el vector de vertices visitados con vertices dobles
+                df.add_vertice(vertice_visitado.clone(), i);
+                i += 1;
+                vertices_visitados.push(vertice_visitado);
+
+                if !aristas_vecinas.is_empty()
+                {
+                    let arista_seleccionada = *aristas_vecinas.get(0).unwrap();
+                    backtrack.push(vertice_visitado);
+                    vertice_visitado = arista_seleccionada.other(vertice_visitado).unwrap();
+                    arbol.add_aristas(vec![arista_seleccionada.clone()]);
                 }
                 else 
                 {
-                    vertice_visitado = arista_elegida.get_vertices().unwrap().0;
-                }
-                vertices_visitados.push(vertice_visitado);
-                df.add_vertice(vertice_visitado.clone(), i);
-                // Incrementamos el contador
-                i += 1;
-                // Asignamos el etiquetado a los vertices adyacentes
-                let adyacentes: Vec<&Vertice> = self.entorno(vertice_visitado).unwrap().into_iter()
-                                            .filter(|x| !vertices_visitados.contains(x))
-                                            .collect();
-                for adyacente in adyacentes.iter()
-                {
-                    match temp.buscar_vertice_mut(adyacente) {
-                        Some(e) => { 
-                            e.set_valor(i);
-                            // Eliminamos la arista antigua
-                            aristas_frontera.retain(|x| !x.arista_contiene_vertice(e.get_vertice()));
-                        },
-                        None => {
-                            temp.add_vertice(adyacente, i);
-                        }
-                    }
-                }                
-                // Actualizamos las aristas frontera
-                let mut extracted = self.aristas_por_vertice(vertice_visitado);
-                aristas_frontera.append(&mut extracted);
-                for vertice in vertices_visitados.iter().filter(|x| **x != vertice_visitado)
-                {
-                    aristas_frontera.retain(|x| !(x.arista_contiene_vertice(vertice)
-                                                                        && x.arista_contiene_vertice(vertice_visitado)));
+                    // Si no hay mas opciones para avanzar, usamos la pila
+                    vertice_visitado = match backtrack.pop() {
+                        Some(e) => e,
+                        None => { break; }
+                    };
                 }
             }
 
